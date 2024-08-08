29e2c844530d3d86777a53c17a9b4db6d1039ac8
BadRAM <dgamebillda@gmail.com>
2024-08-08 14:02:51 -0700

    minor code cleanup

--- barrage.bash
+++ barrage.bash
@@ -16,46 +16,47 @@ git pull
 
 # Check for lockfile
 if [ -f "./locked.txt" ]; then
-	cat ./locked.txt
-else
-    # create and push lockfile
-    echo "Savegame in use by ${USERNAME}" > ./locked.txt 
-    git add locked.txt
-    git commit -m "${USERNAME} Started session" && git push
-     retVal=$?
-    if [ $retVal -ne 0 ]; then
-    	echo "Error detected while pushing lock commit."
-        exit $retVal
-    fi
+    cat ./locked.txt
+    exit 1
+fi
 
-    # move save into game
-    until mv "./${SAVENAME}" "${GAMEDIR}"
-    do 
-        read -p "File move failed! press enter to retry." 
-    done
+# create and push lockfile
+echo "Savegame in use by ${USERNAME}" > ./locked.txt 
+git add locked.txt
+git commit -m "${USERNAME} Started session" && git push
+ retVal=$?
+if [ $retVal -ne 0 ]; then
+	echo "Error detected while pushing lock commit."
+    exit $retVal
+fi
 
-    # inform user and wait for end of session
-    echo "Save locked and loaded! You may now load the game."
-    echo "When you've saved quit the game, write a short log of what you did and press enter."
-    read -p "> " LOG
-    read -p "Confirm game is closed and press enter to push to git and unlock for the next player"
+# move save into game
+until mv "./${SAVENAME}" "${GAMEDIR}"
+do 
+    read -p "File move failed! press enter to retry." 
+done
 
-    # move save back
-    until mv "${GAMEDIR}/${SAVENAME}" "./"
-    do 
-        read -p "File move failed! press enter to retry." 
-    done
+# inform user and wait for end of session
+echo "Save locked and loaded! You may now load the game."
+echo "When you've saved quit the game, write a short log of what you did and press enter."
+read -p "> " LOG
+read -p "Confirm game is closed and press enter to push to git and unlock for the next player"
 
-    # unlock
-    git rm ./locked.txt
-    # rm ./locked.txt
+# move save back
+until mv "${GAMEDIR}/${SAVENAME}" "./"
+do 
+    read -p "File move failed! press enter to retry." 
+done
 
-    # commit changes
-    git add -A *
-    git commit -m "${USERNAME} Finished session - ${LOG}" && git push
-    retVal=$?
-    if [ $retVal -ne 0 ]; then
-    	echo "Error detected while pushing unlock commit. If you can't fix now, please inform the repo owner, or it will be stuck locked."
-        exit $retVal
-    fi
+# unlock
+git rm ./locked.txt
+# rm ./locked.txt
+
+# commit changes
+git add -A *
+git commit -m "${USERNAME} Finished session - ${LOG}" && git push
+retVal=$?
+if [ $retVal -ne 0 ]; then
+	echo "Error detected while pushing unlock commit. If you can't fix now, please inform the repo owner, or it will be stuck locked."
+    exit $retVal
 fi
